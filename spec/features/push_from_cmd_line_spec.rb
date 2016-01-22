222402fac087f67009ab3402a0ce8b211f45caa7
Sam Morgan <s_morgan@me.com>
2016-01-22 14:11:12 +0000

    WIP not too sure where this is going

--- spec/features/push_from_cmd_line_spec.rb
+++ spec/features/push_from_cmd_line_spec.rb
@@ -1,44 +1,41 @@
 require 'git_pusher'
 
 describe 'Pushing to receiver from the command line' do
-  let(:service) { GitPusher.new }
+  
+  # Ensure you have `makersinit` available as a command first
+  context 'with `makersinit` available as a command' do
+    let(:service) { GitPusher.new }
 
-  before do
-    system("cd ..")
-    system("mkdir git_pusher_test")
-    allow(Kernel).to receive(:`).with("git config user.email").and_return "s_morgan@me.com"
+    before do
+      system("mkdir ../git_pusher_test")
+      allow(Kernel).to receive(:`).with("git config user.email").and_return "s_morgan@me.com"
+    end
+
+    it 'sends data to the receiving webhook' do
+      # Expect the final push command with the lovely data
+      expect(service).to receive(:push_to_receiver).with(commits_data)
+      makersinit
+      make_commit
+      push_commit
+    end
+
+    after do
+      system("rm -rf ../git_pusher_test")
+    end
   end
-
-  it 'sends data to the receiving webhook' do
-    # Expect the final push command with the lovely data
-    expect(service).to receive(:push_to_receiver).with(commits_data)
-    makersinit
-    make_commit
-    push_commit
-  end
-
-  # after do
-  #   system("cd ..")
-  #   system("rm -rf git_pusher_test")
-  # end
 
   private
 
   def makersinit
-    system("cd ../git_pusher_test")
-    system("makersinit")
+    system("cd ../git_pusher_test && makersinit")
   end
 
   def make_commit
-    system("cd ../git_pusher_test")
-    system("touch test_file_#{rand(0..58400)}.rb")
-    system("git add .")
-    system("git commit -m 'test commit'")
+    system("cd ../git_pusher_test && touch test_file_#{rand(0..58400)}.rb && git add . && git commit -m 'test commit'")
   end
 
   def push_commit
-    system("cd ../git_pusher_test")
-    system("git push origin master")
+    system("cd ../git_pusher_test && git push origin master")
   end
 
   def commits_data
