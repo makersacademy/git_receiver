2ea49f608ef94357d8d1909decb08482a2955675
Sam Morgan <s_morgan@me.com>
2016-01-22 13:59:31 +0000

    test commit

--- spec/features/push_from_cmd_line_spec.rb
+++ spec/features/push_from_cmd_line_spec.rb
@@ -4,6 +4,7 @@ describe 'Pushing to receiver from the command line' do
   let(:service) { GitPusher.new }
 
   before do
+    system("cd spec/fixtures")
     system("mkdir test")
     system("cd test")
     allow(Kernel).to receive(:`).with("git config user.email").and_return "s_morgan@me.com"
@@ -12,27 +13,32 @@ describe 'Pushing to receiver from the command line' do
   it 'sends data to the receiving webhook' do
     # Expect the final push command with the lovely data
     expect(service).to receive(:push_to_receiver).with(commits_data)
-    # Initialize tracking
     makersinit
-    # Make a commit
     make_commit
-    # Push the commit
     push_commit
+  end
+
+  after do
+    system("cd spec/fixtures")
+    system("rm -rf test")
   end
 
   private
 
   def makersinit
+    system("cd spec/fixtures")
     system("makersinit")
   end
 
   def make_commit
-    system("touch test_file.rb")
+    system("cd spec/fixtures")
+    system("touch test_file_#{rand(0..58400)}.rb")
     system("git add .")
     system("git commit -m 'test commit'")
   end
 
   def push_commit
+    system("cd spec/fixtures")
     system("git push origin master")
   end
 
